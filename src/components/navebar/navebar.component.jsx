02b50551dabad0c20dacd36787b5589194e36109
studioMac-116 <studiomac-116@studioMac-116s-iMac.local>
2022-03-25 18:03:19 +0530

    cart symbol

--- src/components/navebar/navebar.component.jsx
+++ src/components/navebar/navebar.component.jsx
@@ -2,19 +2,26 @@ import React from "react";
 
 import './navebar.styles.scss';
 
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/shopping-bag.svg';
+
 const Navebar = () => {
-    return(
-       <div className="Navebar">
-           <div className="options">
-             <div className="option">Home</div> 
-             <div className="option">Men</div>             
-             <div className="option">Ladies</div>   
-             <div className="option">Kids</div>   
-             <div className="option">Accessories</div>   
-             <div className="option">Blog</div>
-             <div className="option">About us</div>    
-           </div>
-       </div>
+    return (
+        <div className="Navebar">
+            <Link className="logo-container" to='/'>
+                <Logo className="logo" />
+            </Link>
+            <div className="options">
+                <div className="option">Home</div>
+                <div className="option">Men</div>
+                <div className="option">Ladies</div>
+                <div className="option">Kids</div>
+                <div className="option">Accessories</div>
+                <div className="option">Blog</div>
+                <div className="option">About us</div>
+            </div>
+        </div>
     );
 };
 
