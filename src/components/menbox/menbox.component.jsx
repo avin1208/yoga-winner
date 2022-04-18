1afc5295fa698650491ea3224de3234792430f80
studioMac-116 <studiomac-116@studioMac-116s-iMac.local>
2022-04-18 18:03:31 +0530

    Accordian Completed

--- src/components/menbox/menbox.component.jsx
+++ src/components/menbox/menbox.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -6,7 +6,10 @@ import './menbox.styles.scss';
 
 import { FaPlusSquare } from 'react-icons/fa';
 
+import { FaMinusSquare } from 'react-icons/fa';
+
 const Menbov = () => {
+    const [show, setShow] = useState(false);
     return (
         <div className="menbov">
             <div className="menbov-title">
@@ -14,13 +17,93 @@ const Menbov = () => {
                     Home
                 </Link>
                 <Link className="women-bov">
-                    Women <FaPlusSquare className="squre" />
+                    Women <div className="jkl" onClick={() => setShow(!show)}>
+                        {show ? <FaMinusSquare /> : <FaPlusSquare />}
+                    </div>
+                    {show &&
+                        <div className="women-acc">
+                            <Link className="toppp">
+                                Top <FaPlusSquare />
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Dresses <FaPlusSquare />
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Handbags <FaPlusSquare />
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Shoes
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Clothing <FaPlusSquare />
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Accessories
+                            </Link>
+                        </div>
+                    }
                 </Link>
+              
                 <Link className="mens-bov">
-                    Men <FaPlusSquare className="squre" />
+                    Men <div className="jkn" onClick={() => setShow(!show)}>
+                        {show ? <FaMinusSquare /> : <FaPlusSquare />}
+                    </div>
+                    {show &&
+                        <div className="women-acc">
+                            <Link className="toppp">
+                                Clothing
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Shoes
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Bags
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Lingerie
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Best Seller
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                Collction
+                                <hr color="#ebebeb" />
+                            </Link>
+                            <Link className="toppp">
+                                trending
+                            </Link>
+                        </div>
+                    }
                 </Link>
                 <Link className="fashion-bov">
-                    Fashion <FaPlusSquare className="squre" />
+                    Fashion <div className="jkn" onClick={() => setShow(!show)}>
+                    {show ? <FaMinusSquare /> : <FaPlusSquare />}
+                </div>
+                { show && 
+                    <div className="women-acc">
+                        <Link className="toppp">
+                            Accessories <FaPlusSquare />
+                            <hr color="#ebebeb" />
+                        </Link>
+                        <Link className="toppp">
+                            Footware <FaPlusSquare />
+                            <hr color="#ebebeb" />
+                        </Link>
+                        <Link className="toppp">
+                            Kids Fashion <FaPlusSquare />
+                        </Link>
+                    </div>
+                }
                 </Link>
             </div>
         </div>
